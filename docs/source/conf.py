8ec6ad0991523e3ad622f59a12e58ba98492d26b
greysonlalonde <greyson.r.lalonde@gmail.com>
2022-06-16 09:19:49 -1000

    Update conf.py
    make docs path work with src-layout

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -12,19 +12,18 @@
 #
 import os
 import sys
-sys.path.insert(0, os.path.abspath('../..'))
 
+sys.path.insert(0, os.path.abspath("../../src"))
 
 # -- Project information -----------------------------------------------------
 
-project = 'Sweetpotato'
-copyright = '2022, Greyson R. LaLonde'
-author = 'Greyson R. LaLonde'
+project = "Sweetpotato"
+copyright = "2022, Greyson R. LaLonde"
+author = "Greyson R. LaLonde"
 
 # The full version, including alpha/beta/rc tags
-release = 'v0.1-alpha'
-version = 'v0.1-alpha'
-
+release = "v0.1-alpha"
+version = "v0.1-alpha"
 
 # -- General configuration ---------------------------------------------------
 
@@ -32,14 +31,14 @@ version = 'v0.1-alpha'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
-    'sphinx.ext.autodoc',
-    'sphinx.ext.viewcode',
-    'sphinx.ext.napoleon',
-    'sphinx.ext.duration',
-    'sphinx.ext.doctest',
-    'sphinx.ext.autosummary',
-    'sphinx.ext.intersphinx',
-    'sphinx.ext.todo',
+    "sphinx.ext.autodoc",
+    "sphinx.ext.viewcode",
+    "sphinx.ext.napoleon",
+    "sphinx.ext.duration",
+    "sphinx.ext.doctest",
+    "sphinx.ext.autosummary",
+    "sphinx.ext.intersphinx",
+    "sphinx.ext.todo",
     "sphinx_rtd_dark_mode",
 ]
 
@@ -53,39 +52,33 @@ autosummary_generate = True
 todo_include_todos = True
 
 # Add any paths that contain templates here, relative to this directory.
-templates_path = ['_templates']
-
+templates_path = ["_templates"]
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
 exclude_patterns = []
 
-
 # -- Options for HTML output -------------------------------------------------
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'sphinx_rtd_theme'
-
+html_theme = "sphinx_rtd_theme"
 
 intersphinx_mapping = {
-    'python': ('https://docs.python.org/3/', None),
-    'sphinx': ('https://www.sphinx-doc.org/en/master/', None),
+    "python": ("https://docs.python.org/3/", None),
+    "sphinx": ("https://www.sphinx-doc.org/en/master/", None),
 }
 
 autodoc_typehints = "description"
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
-
-
+html_static_path = ["_static"]
 
 html_theme_options = {
-    'logo_only': True,
-
+    "logo_only": True,
 }
 default_light_mode = True
 
