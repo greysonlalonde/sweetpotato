8e090b16ec2433134d87025be1c55bc1dfe89966
greysonlalonde <greyson.r.lalonde@gmail.com>
2022-06-28 06:10:44 -1000

    Update context_wrappers.py
    adding context wrappers to handle authentication, navigation, & UI kitten settings

--- src/sweetpotato/core/context_wrappers.py
+++ src/sweetpotato/core/context_wrappers.py
@@ -1,72 +1,67 @@
+"""
+Todo:
+    * Add docstrings for all classes & methods.
+    * Add typing.
+"""
 from abc import abstractmethod, ABC
 from typing import List
 
-from sweetpotato.config import settings
-from sweetpotato.core.protocols import Composite
-from sweetpotato.navigation import NavigationContainer
-from sweetpotato.ui_kitten import ApplicationProvider
 from sweetpotato.authentication import AuthenticationProvider
 from sweetpotato.components import SafeAreaProvider
+from sweetpotato.config import settings
+from sweetpotato.navigation import NavigationContainer
+from sweetpotato.ui_kitten import ApplicationProvider
 
 
-class Wrapper(ABC):
+class AbstractWrapper(ABC):
     @abstractmethod
-    def set_next(self, wrapper: "Wrapper") -> "Wrapper":
+    def set_next(self, wrapper) -> None:
         raise NotImplementedError
 
     @abstractmethod
-    def wrap(self, component) -> Composite:
+    def wrap(self, component) -> None:
         raise NotImplementedError
 
 
-class AbstractWrapper(Wrapper):
-    _next_wrapper: Wrapper = None
+class Wrapper(AbstractWrapper):
+    _next_wrapper: "Wrapper" = None
 
-    def set_next(self, wrapper: Wrapper) -> Wrapper:
-        self._next_wrapper = wrapper
-        return wrapper
+    def set_next(self, wrapper: "Wrapper") -> None:
+
+        if not self._next_wrapper:
+            self._next_wrapper = wrapper
+        else:
+            self._next_wrapper.set_next(wrapper)
 
     @abstractmethod
-    def wrap(self, component: Composite) -> Composite:
+    def wrap(self, component):
         if self._next_wrapper:
             return self._next_wrapper.wrap(component)
         component.is_root = True
         return component
 
 
-class UIKittenWrapper(AbstractWrapper):
-    def wrap(self, component: Composite) -> SafeAreaProvider:
+class UIKittenWrapper(Wrapper):
+    def wrap(self, component):
         if settings.USE_UI_KITTEN:
             component = ApplicationProvider(children=[component])
         return super().wrap(component)
 
 
-class AuthenticationWrapper(AbstractWrapper):
-    def wrap(self, component: Composite) -> Composite:
+class AuthenticationWrapper(Wrapper):
+    def wrap(self, component):
         if settings.USE_AUTHENTICATION:
             component = AuthenticationProvider(children=[component])
         return super().wrap(component)
 
 
-class NavigationWrapper(AbstractWrapper):
-    def wrap(self, component: Composite) -> Composite:
+class NavigationWrapper(Wrapper):
+    def wrap(self, component):
         if settings.USE_NAVIGATION:
             component = NavigationContainer(children=[component], ref="navigationRef")
         return super().wrap(component)
 
 
-class ContextWrapper(AbstractWrapper):
-    def __init__(self):
-        self.set_next(
-            AuthenticationWrapper()
-        ).set_next(
-            NavigationWrapper()
-        ).set_next(
-            UIKittenWrapper()
-        )
-
-    def wrap(self, component: List) -> Composite:
-        component = SafeAreaProvider(children=component)
-        context = super().wrap(component)
-        print(context)
-        return context
+class ContextWrapper(AuthenticationWrapper, UIKittenWrapper, NavigationWrapper):
+    def wrap(self, component: List):
+        return super().wrap(SafeAreaProvider(children=component))
