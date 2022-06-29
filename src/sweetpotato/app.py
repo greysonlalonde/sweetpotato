0f23406b134b00334f58c67cf1cbfda56d0e31a9
greysonlalonde <greyson.r.lalonde@gmail.com>
2022-06-29 00:05:43 -1000

    Update app.py

--- src/sweetpotato/app.py
+++ src/sweetpotato/app.py
@@ -1,36 +1,26 @@
-import subprocess
 from typing import Optional, List
-
-from sweetpotato.config import settings
 from sweetpotato.core.build import Build
 from sweetpotato.core.context_wrappers import ContextWrapper
 from sweetpotato.core.utils import (
-    Renderer,
-    Importer,
-    AppProxy,
+    ComponentRenderer,
+    ApplicationRenderer,
+    ImportRenderer,
+    Storage,
 )
-
-proxy = AppProxy()
 
 
 class App:
     context = ContextWrapper()
     build = Build()
+    renderers = (ComponentRenderer, ImportRenderer, ApplicationRenderer,)
 
     def __init__(self, children: Optional[List] = None) -> None:
         super().__init__()
         if children is None:
             children = []
         self.context = self.context.wrap(children)
-        self.context.register(visitor=Renderer)
-        self.context.register(visitor=Importer)
-
-        l = "{"
-        r = "}"
-        imports = f"import React from 'react';\n{proxy.internals['App']['imports']}"
-        app_repr = f"{imports}export default class App extends React.Component {l} render(){l}return( {self.context.rendition} ){r}{r}"
-        with open("App.js", "w") as file:
-            file.write(app_repr)
+        for renderer in self.renderers:
+            self.context.register(visitor=renderer)
 
     def run(self, platform: Optional[str] = None) -> None:
         """Starts a React Native expo client through a subprocess.
